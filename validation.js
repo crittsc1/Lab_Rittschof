58a0e6ca6ea09d39a57d943944058d229368e459
Collin Rittschof <crittsc1@my.westga.edu>
2024-10-18 11:00:09 -0400

    Created validation.js and wrote inline javascript on the homepage. linked validation.js to the Reservations.html page  Changes to be committed: 	modified:   Reservations.html 	modified:   index.html 	modified:   pacific.css 	new file:   validation.js

---
+++ validation.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    var lname = document.getElementById("lname").value;
+    if (lname === "") {
+        alert("Please enter a Last name.")
+        return false
+    }
+    else {
+        return true
+    }
+}
